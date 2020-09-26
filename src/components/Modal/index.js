b44412060b60795051a83d0a24b2bd4e330740a8
Brandon Jackson <brandonljackson23@gmail.com>
2020-09-26 10:34:22 -0600

    modal tests

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -10,9 +10,7 @@ function Modal({ onClose, currentPhoto }) {
                 <h3 className="modalTitle">{name}</h3>
                 <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
                 <p>{description}</p>
-                <button onClick={onClose} type="button">
-                    Close this modal
-                </button>
+                <button onClick={onClose} type="button">Close this modal</button>
             </div>
         </div>
     );
